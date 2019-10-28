7bcab1ea197dd4ea034e4f1ddfe6e2bf2aeff776
Reed James <reedtjames1234@gmail.com>
2019-10-28 10:15:09 -0500

    Video 1, check if chain is valid

--- blockchain.js
+++ blockchain.js
@@ -11,4 +11,20 @@ class Blockchain {
 
     return block;
   }
+
+  isValidChain(chain) {
+    // 3 conditions for a chain to be valid:
+    // first block should be a genisis block, the prevHash of the current block is the hash of the previous block,
+    // the hash of
+    // check to see if first block is genesis block
+    // if(JSON.stringify(this.chain[0])===JSON.stringify(Block.genesis()))
+    // return false;
+    // for(let i = 1; 1<this.chain.length; i++){
+    //   const prevBlock = this.chain[i-i]
+    //   const block = this.chain[i]
+    //   if(block.prevHash !== prevBlock.hash || )
+    // }
+  }
 }
+
+module.exports = Blockchain;
